b68c10ec86cca0be09ff08af57548d4f65f65296
F1D02310100_Apriesna Zulhan <apriesnazulhan1229@gmail.com>
2024-12-21 16:30:23 +0800

    Update Final Main and User

---
+++ src/User.java
@@ -0,0 +1,12 @@
+// User.java
+class User {
+    String username;
+    String password;
+    int score;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.score = 0;
+    }
+}
